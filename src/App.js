01ff86a56e95e87db71de2e23ae5a6eb6b229973
dungpt99 <phungdung080299@gmail.com>
2021-12-27 10:39:46 +0700

    fix6

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Home from "./pages/home/Home.jsx";
 import Login from "./pages/login/Login.jsx";
 import Profile from "./pages/profile/Profile.jsx";
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,
   Switch,
   Route,
   Link,
--- src/index.js
+++ src/index.js
@@ -3,14 +3,11 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
 import { AuthContextProvider } from "./context/AuthContext";
-import { HashRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
     <AuthContextProvider>
-      <HashRouter>
-        <App />
-      </HashRouter>
+      <App />
     </AuthContextProvider>
   </React.StrictMode>,
   document.getElementById("root")
