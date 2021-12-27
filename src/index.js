4789649ffa188bd0a7dfdad37e9927d40054fbeb
dungpt99 <phungdung080299@gmail.com>
2021-12-27 10:17:09 +0700

    fix router

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
 import { AuthContextProvider } from "./context/AuthContext";
+import { HashRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <AuthContextProvider>
-      <App />
-    </AuthContextProvider>
+    <HashRouter>
+      <AuthContextProvider>
+        <App />
+      </AuthContextProvider>
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
