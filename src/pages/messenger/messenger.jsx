0ebdbc6f7879d0449d7a39f0cb3107b021843971
dungpt99 <phungdung080299@gmail.com>
2021-12-27 13:48:32 +0700

    fix cor

--- src/pages/messenger/messenger.jsx
+++ src/pages/messenger/messenger.jsx
@@ -16,7 +16,7 @@ export default function Messenger() {
   const [arrivalMessage, setArrivalMessage] = useState(null);
   const [onlineUser, setOnlineUser] = useState(null);
   const [socket, setSocket] = useState(
-    io("ws://socialsocketio122021.herokuapp.com")
+    io("wss://socialsocketio122021.herokuapp.com", { secure: true })
   );
 
   const { user } = useContext(AuthContext);
